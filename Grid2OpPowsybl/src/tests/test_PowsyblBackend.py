4bc3fa849833f08b597a4e7e9ae7949aa76211f9
rtschupp <remi.tschupp@gmail.com>
2023-05-24 11:29:32 +0200

    Refactoring for src to be a module and not a python project directory

--- Grid2OpPowsybl/src/tests/test_PowsyblBackend.py
+++ Grid2OpPowsybl/src/tests/test_PowsyblBackend.py
@@ -8,17 +8,19 @@
 
 import unittest
 import warnings
-
+import sys
 import numpy as np
 import os
 from grid2op import make
-
 from grid2op.tests.helper_path_test import PATH_DATA_TEST_PP, PATH_DATA_TEST
-from grid2op.Backend.PowsyblBackend import PowsyblBackend
+# from src
+from src.Backend.PowsyblBackend import PowsyblBackend
+# import Backend.PowsyblBackend
+# from src.Backend.PowsyblBackend import PowsyblBackend
 
 from grid2op.tests.helper_path_test import HelperTests
-from grid2op.tests.BaseBackendTestPyPowsybl import BaseTestNames
-from grid2op.tests.BaseBackendTestPyPowsybl import BaseTestLoadingCase
+from BaseBackendTestPyPowsybl import BaseTestNames
+from BaseBackendTestPyPowsybl import BaseTestLoadingCase
 # from grid2op.tests.BaseBackendTest import BaseTestNames
 # from grid2op.tests.BaseBackendTest import BaseTestLoadingCase
 from grid2op.tests.BaseBackendTest import BaseTestLoadingBackendFunc
